20e8293a05bd34b5006a3349ec081b3109fa8872
Daniel Succar <danielsuccar@icloud.com>
2019-12-16 21:30:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ return ("Happy holidays!")
 }
 
 function happyHolidaysTo(name){
-  return `Happy holidays, ${name}!`
+  console.log `Happy holidays, ${name}!`
 }
 
 function happyHolidaysTo(holiday, name){
